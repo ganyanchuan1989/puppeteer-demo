955838b0d5f76fe6714ada6c6eb46c75fe4b53e5
ganxunzou <ganxunzou@163.com>
2019-11-03 13:27:04 +0800

    add .

--- src/doudada.js
+++ src/doudada.js
@@ -14,49 +14,68 @@ const dl = require("./dl");
   await page.click(".login-btn");
   await page.screenshot({ path: "c:/tmp/ddd.png" });
   await page.waitForNavigation({ waitUntil: "networkidle0" });
-  await page.goto("https://www.doudada.com/hot/video");
 
-  const childPage = async url => {
-    const subPage = await browser.newPage();
-    await subPage.goto(url);
-    const content = await subPage.content();
-    const idex = content.indexOf(',url:"');
-    if (idex > 0) {
-      let url = content.substring(
-        idex + 6,
-        content.indexOf(",collectionTime:") - 1
-      );
-
-      url = url.replace(/u002f/gi, "");
-      // console.log(url);
-      const dyPage = await browser.newPage();
-      await dyPage.goto(url);
-      const dyContent = await dyPage.content();
-      const dyIdex = dyContent.indexOf("playAddr:");
-      // console.log(dyIdex);
-      if (dyIdex > -1) {
-        const tempStr = dyContent.substr(dyIdex + 11);
-        // console.log("tempStr:", tempStr);
-        const dyVUrl = tempStr.substr(0, tempStr.indexOf('"'));
-        // console.log("dyVUrl:", dyVUrl);
-        dl(dyVUrl);
-        try {
-          // dyPage.close();
-          // subPage.close();
-        } catch (error) {}
-      }
-    }
+  const sleep = async time => {
+    return new Promise(function(resolve) {
+      setTimeout(resolve, time);
+    });
   };
 
-  const aurls = await page.$$eval(".video-box", items => {
-    const urls = items.map(item => {
-      return item.firstElementChild.href;
-    });
-    return urls;
-  });
+  const downloadByPage = async url => {
+    await page.goto(url);
 
-  aurls.forEach(async url => {
-    childPage(url);
-  });
-  // childPage(aurls[0]);
+    const childPage = async url => {
+      const subPage = await browser.newPage();
+      await subPage.goto(url);
+      const content = await subPage.content();
+      const idex = content.indexOf(',url:"');
+      if (idex > 0) {
+        let url = content.substring(
+          idex + 6,
+          content.indexOf(",collectionTime:") - 1
+        );
+
+        url = url.replace(/u002f/gi, "");
+        // console.log(url);
+        const dyPage = await browser.newPage();
+        await dyPage.goto(url);
+        const dyContent = await dyPage.content();
+        const dyIdex = dyContent.indexOf("playAddr:");
+        // console.log(dyIdex);
+        if (dyIdex > -1) {
+          const tempStr = dyContent.substr(dyIdex + 11);
+          // console.log("tempStr:", tempStr);
+          const dyVUrl = tempStr.substr(0, tempStr.indexOf('"'));
+          // console.log("dyVUrl:", dyVUrl);
+          dl(dyVUrl);
+          try {
+            dyPage.close();
+            subPage.close();
+          } catch (error) {}
+        }
+      }
+    };
+
+    const aurls = await page.$$eval(".video-box", items => {
+      const urls = items.map(item => {
+        return item.firstElementChild.href;
+      });
+      return urls;
+    });
+
+    aurls.forEach(async url => {
+      childPage(url);
+      await sleep(2000);
+    });
+  };
+
+  for (let i = 1; i <= 5; i++) {
+    await downloadByPage(
+      "https://www.doudada.com/hot/video?keyword=&page=" +
+        i +
+        "&interval=1&existGoods=false&sort=0"
+    );
+
+    await sleep(5000);
+  }
 })();
--- src/test.js
+++ src/test.js
@@ -1,2 +1,16 @@
-const moment = require("moment");
-console.log(moment());
+// (async function() {
+//   const sleep = async time => {
+//     return new Promise(function(resolve) {
+//       setTimeout(resolve, time);
+//     });
+//   };
+
+//   await sleep(5000);
+//   console.log(">>>");
+// })();
+
+for (let i = 1; i <= 5; i++) {
+  console.log(
+    `https://www.doudada.com/hot/video?keyword=&page=${i}&interval=1&existGoods=false&sort=0`
+  );
+}
